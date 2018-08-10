7527d78d7f344ff68df17cbc3605766f5cad9b5c
CamilaEspinozaMoya <camila.andrea.espinoza@gmail.com>
2018-08-10 09:02:58 -0400

    avanzando

---
+++ public/src/js/login.js
@@ -0,0 +1,57 @@
+/*
+window.onload = () =>{
+  firebase.auth().onAuthStateChanged((user)=>{
+    if(user){
+    //Si estamos logueados
+    paginaRegistro.style.display = "none";
+    menuPrincipal.style.display = "block";
+    console.log("User > "+JSON.stringify(user));
+    }else{
+    //No estamos logueados
+    paginaRegistro.style.display = "block";
+    menuPrincipal.style.display = "none";
+    }
+  });
+};
+*/
+
+function register(){
+  let email = document.getElementById('email1').value;
+  let password = document.getElementById('password1').value;
+  console.log(email); 
+  firebase.auth().createUserWithEmailAndPassword(email, password)
+  .then(()=>{
+    console.log("Usuario registrado");
+    alert("Registro exitoso, ahora puedes iniciar sesión");
+  })
+  .catch(function(error){
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    console.log("Error de firebase"+error.code);
+    console.log("Error de firebase, mensaje"+error.message);
+  });
+}
+
+function login(){
+  const email = document.getElementById('email1').value;
+  const password = document.getElementById('password1').value; 
+  firebase.auth().signInWithEmailAndPassword(email, password)
+  .then(()=>{
+    console.log("Usuario con login exitoso");
+  })
+  .catch((error)=>{
+    console.log("Error de firebase"+error.code);
+    console.log("Error de firebase, mensaje"+error.message);
+  });
+}
+
+function logout(){
+  firebase.auth().signOut()
+  .then(()=>{
+    console.log("Usuario finalizó su sesión");
+  })
+  .catch((error)=>{
+    console.log("Error de firebase Código"+error.code); //error.code nos mostrará el código de error para informarnos qué pasó
+    console.log("Error de firebase Mensaje"+error.message); //error.message nos mostrará el mensaje de firebase del mismo error
+});
+}
